b209d92a0d17f87dcb183ce33b7ce6dfac0f4ab8
Okonai <kovacs.ajtai.tamas@gmail.com>
2022-10-16 18:51:43 +0200

    Fix/refactoring (#4)
    * fix: Reduce class method complexity
    
    * fix: delete duplicated code, simplify business logc

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -1,59 +1,78 @@
 class Item {
-  constructor(name, sellIn, quality){
+  constructor(name, sellIn, quality) {
     this.name = name;
     this.sellIn = sellIn;
     this.quality = quality;
   }
 }
 
+const ItemTypes = {
+  BIRE: "Aged Brie",
+  HAND: "Sulfuras, Hand of Ragnaros",
+  PASS: "Backstage passes to a TAFKAL80ETC concert",
+};
+
+function updateBrie(item) {
+  if (item.quality < 50) {
+    item.quality = item.quality + 1;
+  }
+  item.sellIn = item.sellIn - 1;
+
+  if (item.sellIn < 0 && item.quality < 50) {
+    item.quality = item.quality + 1;
+  }
+}
+
+function updatePass(item) {
+  if (item.quality < 50) {
+    item.quality = item.quality + 1;
+    if (item.sellIn < 11 && item.quality < 50) {
+      item.quality = item.quality + 1;
+    }
+    if (item.sellIn < 6 && item.quality < 50) {
+      item.quality = item.quality + 1;
+    }
+  }
+
+  item.sellIn = item.sellIn - 1;
+
+  if (item.sellIn < 0 ) {
+    item.quality = item.quality - item.quality;
+  }
+}
+
+function updateHand(item) {
+}
+
+function updateNormal(item) {
+  if (item.quality > 0) {
+    item.quality = item.quality - 1;
+  }
+  item.sellIn = item.sellIn - 1;
+  if (item.sellIn < 0 && item.quality > 0) {
+        item.quality = item.quality - 1;
+      }
+}
+
 class Shop {
-  constructor(items=[]){
+  constructor(items = []) {
     this.items = items;
   }
   updateQuality() {
-    for (var i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
-        }
+    for (const item of this.items) {
+      switch (item.name) {
+        case ItemTypes.BIRE:
+          updateBrie(item);
+          continue;
+        case ItemTypes.HAND:
+          updateHand(item);
+          continue;
+        case ItemTypes.PASS:
+          updatePass(item);
+          continue;
+        default:
+          updateNormal(item);
+          continue;
       }
     }
 
@@ -62,5 +81,5 @@ class Shop {
 }
 module.exports = {
   Item,
-  Shop
-}
+  Shop,
+};
