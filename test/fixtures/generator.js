adc5c716912ef5f4748c38ad1ee6b1636b736976
Okonai <kovacs.ajtai.tamas@gmail.com>
2022-10-16 12:26:56 +0300

    Test/characterization tests (#3)
    * chore: add requirements file
    
    * chore: Test data generator script
    
    * test: Generate test data and write test for it, add nppm scripts

---
+++ test/fixtures/generator.js
@@ -0,0 +1,54 @@
+const { Shop, Item } = require("../../src/gilded_rose");
+const fs = require("fs");
+
+// We have the following items defined in the source code, and we want to test them all:
+const names = [
+  "Aged Brie",
+  "Sulfuras, Hand of Ragnaros",
+  "Backstage passes to a TAFKAL80ETC concert",
+  "+5 Dexterity Vest",
+];
+
+// Since the sellIn and quality values are can be between 0-12 and 0-50 respectively, we make sure we include all possible values in the test cases.
+const [minSellIn, maxSellIn] = [-1, 12];  
+const [minQuality, maxQuality] = [-1, 51];
+
+const tests = [];
+
+// We want to test all possible combinations of the items, sellIn and quality values.
+for (const name of names) {
+  for (let sellIn = minSellIn; sellIn <= maxSellIn; sellIn++) {
+    for (let quality = minQuality; quality <= maxQuality; quality++) {
+
+
+      const guildedRose = new Shop([new Item(name, sellIn, quality)]);
+      const items = guildedRose.updateQuality();
+
+      const outputSellIn = items[0].sellIn;
+      const outputQuality = items[0].quality;
+
+      tests.push({
+        name, 
+        sellIn,
+        quality,
+
+        expectedSellIn: outputSellIn,
+        expectedQuality: outputQuality,
+      })
+      
+    }
+  }
+}
+
+const outputFilename = "test/fixtures/characterized_test_data.json";
+
+// We write the test cases to a file so that we can use them in our test suite. We will use the file in the characterized tests later.
+fs.writeFileSync(outputFilename, JSON.stringify(tests, null, 2));
+
+// We give feedback to the user how many test cases we have generated.
+console.log(`Generated ${tests.length} test cases. into ${outputFilename}`);
+
+module.exports = {
+  tests,
+  outputFilename
+}
--- test/test_gilded_rose.js
+++ test/test_gilded_rose.js
@@ -1,11 +1,30 @@
-var {expect} = require('chai');
-var {Shop, Item} = require('../src/gilded_rose.js');
-describe("Gilded Rose", function() {
+var { expect } = require("chai");
+var { Shop, Item } = require("../src/gilded_rose.js");
 
-  it("should foo", function() {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
+const characterisedTests = require("./fixtures/characterized_test_data.json");
+
+describe("Gilded Rose charactrized tests", function () {
+
+  // We loop over all the test cases we generated with the generator script.
+  for(const test of characterisedTests) {
+    
+    const { name, sellIn, quality, expectedSellIn, expectedQuality } = test;
+    const description = `should update ${name} with sellIn ${sellIn} and quality ${quality} to sellIn ${expectedSellIn} and quality ${expectedQuality}`;
+
+    it(description, function () {
+      const gildedRose = new Shop([new Item(name, sellIn, quality)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].name).to.equal(name);
+      expect(items[0].sellIn).to.equal(expectedSellIn);
+      expect(items[0].quality).to.equal(expectedQuality);
+    });
+  }
+});
+
+describe("Gilded Rose", function () {
+  it("should foo", function () {
+    const gildedRose = new Shop([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).to.equal("fixme");
+    expect(items[0].name).to.equal("foo");
   });
-
 });
