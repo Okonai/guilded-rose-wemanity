3c328130f40c0a2503c805fa0b2f6f28be545537
Okonai <kovacs.ajtai.tamas@gmail.com>
2022-10-16 20:08:52 +0200

    chore: remove obsolate test

--- test/test_gilded_rose.js
+++ test/test_gilded_rose.js
@@ -1,32 +1,14 @@
 var { expect } = require("chai");
-var { test, describe, it } = require("mocha");
 var { Shop, Item } = require("../src/gilded_rose.js");
 
-const characterisedTests = require("./fixtures/characterized_test_data.json");
-
-xdescribe("Gilded Rose charactrized tests", function () {
-  // We loop over all the test cases we generated with the generator script.
-  for (const test of characterisedTests) {
-    const { name, sellIn, quality, expectedSellIn, expectedQuality } = test;
-    const description = `should update ${name} with sellIn ${sellIn} and quality ${quality} to sellIn ${expectedSellIn} and quality ${expectedQuality}`;
-
-    it(description, function () {
-      const gildedRose = new Shop([new Item(name, sellIn, quality)]);
-      const items = gildedRose.updateQuality();
-      expect(items[0].name).to.equal(name);
-      expect(items[0].sellIn).to.equal(expectedSellIn);
-      expect(items[0].quality).to.equal(expectedQuality);
-    });
-  }
-});
+const ItemTypes = {
+  BRIE: "Aged Brie",
+  HAND: "Sulfuras, Hand of Ragnaros",
+  PASS: "Backstage passes to a TAFKAL80ETC concert",
+  NORMAL: "+5 Dexterity Vest",
+};
 
 describe("Gilded Rose", function () {
-  const ItemTypes = {
-    BRIE: "Aged Brie",
-    HAND: "Sulfuras, Hand of Ragnaros",
-    PASS: "Backstage passes to a TAFKAL80ETC concert",
-    NORMAL: "+5 Dexterity Vest",
-  };
 
   describe("updateQuality", function () {
     describe(ItemTypes.NORMAL, function () {
