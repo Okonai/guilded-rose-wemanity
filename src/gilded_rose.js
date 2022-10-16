6e4b63a86936f9a4b518df419056def94993ab00
Okonai <kovacs.ajtai.tamas@gmail.com>
2022-10-16 18:36:51 +0200

    fix: delete duplicated code, simplify business logc

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -13,199 +13,45 @@ const ItemTypes = {
 };
 
 function updateBrie(item) {
-  if (
-    item.name != "Aged Brie" &&
-    item.name != "Backstage passes to a TAFKAL80ETC concert"
-  ) {
-    if (item.quality > 0) {
-      if (item.name != "Sulfuras, Hand of Ragnaros") {
-        item.quality = item.quality - 1;
-      }
-    }
-  } else {
-    if (item.quality < 50) {
-      item.quality = item.quality + 1;
-      if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-        if (item.sellIn < 11) {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-          }
-        }
-        if (item.sellIn < 6) {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-          }
-        }
-      }
-    }
+  if (item.quality < 50) {
+    item.quality = item.quality + 1;
   }
-  if (item.name != "Sulfuras, Hand of Ragnaros") {
-    item.sellIn = item.sellIn - 1;
-  }
-  if (item.sellIn < 0) {
-    if (item.name != "Aged Brie") {
-      if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
-        if (item.quality > 0) {
-          if (item.name != "Sulfuras, Hand of Ragnaros") {
-            item.quality = item.quality - 1;
-          }
-        }
-      } else {
-        item.quality = item.quality - item.quality;
-      }
-    } else {
-      if (item.quality < 50) {
-        item.quality = item.quality + 1;
-      }
-    }
-  }
-}
+  item.sellIn = item.sellIn - 1;
 
-function updateHand(item) {
-  if (
-    item.name != "Aged Brie" &&
-    item.name != "Backstage passes to a TAFKAL80ETC concert"
-  ) {
-    if (item.quality > 0) {
-      if (item.name != "Sulfuras, Hand of Ragnaros") {
-        item.quality = item.quality - 1;
-      }
-    }
-  } else {
-    if (item.quality < 50) {
-      item.quality = item.quality + 1;
-      if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-        if (item.sellIn < 11) {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-          }
-        }
-        if (item.sellIn < 6) {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-          }
-        }
-      }
-    }
-  }
-  if (item.name != "Sulfuras, Hand of Ragnaros") {
-    item.sellIn = item.sellIn - 1;
-  }
-  if (item.sellIn < 0) {
-    if (item.name != "Aged Brie") {
-      if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
-        if (item.quality > 0) {
-          if (item.name != "Sulfuras, Hand of Ragnaros") {
-            item.quality = item.quality - 1;
-          }
-        }
-      } else {
-        item.quality = item.quality - item.quality;
-      }
-    } else {
-      if (item.quality < 50) {
-        item.quality = item.quality + 1;
-      }
-    }
+  if (item.sellIn < 0 && item.quality < 50) {
+    item.quality = item.quality + 1;
   }
 }
 
 function updatePass(item) {
-  if (
-    item.name != "Aged Brie" &&
-    item.name != "Backstage passes to a TAFKAL80ETC concert"
-  ) {
-    if (item.quality > 0) {
-      if (item.name != "Sulfuras, Hand of Ragnaros") {
-        item.quality = item.quality - 1;
-      }
-    }
-  } else {
-    if (item.quality < 50) {
+  if (item.quality < 50) {
+    item.quality = item.quality + 1;
+    if (item.sellIn < 11 && item.quality < 50) {
       item.quality = item.quality + 1;
-      if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-        if (item.sellIn < 11) {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-          }
-        }
-        if (item.sellIn < 6) {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-          }
-        }
-      }
+    }
+    if (item.sellIn < 6 && item.quality < 50) {
+      item.quality = item.quality + 1;
     }
   }
-  if (item.name != "Sulfuras, Hand of Ragnaros") {
-    item.sellIn = item.sellIn - 1;
-  }
-  if (item.sellIn < 0) {
-    if (item.name != "Aged Brie") {
-      if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
-        if (item.quality > 0) {
-          if (item.name != "Sulfuras, Hand of Ragnaros") {
-            item.quality = item.quality - 1;
-          }
-        }
-      } else {
-        item.quality = item.quality - item.quality;
-      }
-    } else {
-      if (item.quality < 50) {
-        item.quality = item.quality + 1;
-      }
-    }
+
+  item.sellIn = item.sellIn - 1;
+
+  if (item.sellIn < 0 ) {
+    item.quality = item.quality - item.quality;
   }
 }
 
+function updateHand(item) {
+}
+
 function updateNormal(item) {
-  if (
-    item.name != "Aged Brie" &&
-    item.name != "Backstage passes to a TAFKAL80ETC concert"
-  ) {
-    if (item.quality > 0) {
-      if (item.name != "Sulfuras, Hand of Ragnaros") {
+  if (item.quality > 0) {
+    item.quality = item.quality - 1;
+  }
+  item.sellIn = item.sellIn - 1;
+  if (item.sellIn < 0 && item.quality > 0) {
         item.quality = item.quality - 1;
       }
-    }
-  } else {
-    if (item.quality < 50) {
-      item.quality = item.quality + 1;
-      if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-        if (item.sellIn < 11) {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-          }
-        }
-        if (item.sellIn < 6) {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-          }
-        }
-      }
-    }
-  }
-  if (item.name != "Sulfuras, Hand of Ragnaros") {
-    item.sellIn = item.sellIn - 1;
-  }
-  if (item.sellIn < 0) {
-    if (item.name != "Aged Brie") {
-      if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
-        if (item.quality > 0) {
-          if (item.name != "Sulfuras, Hand of Ragnaros") {
-            item.quality = item.quality - 1;
-          }
-        }
-      } else {
-        item.quality = item.quality - item.quality;
-      }
-    } else {
-      if (item.quality < 50) {
-        item.quality = item.quality + 1;
-      }
-    }
-  }
 }
 
 class Shop {
